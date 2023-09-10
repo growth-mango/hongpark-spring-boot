9256bcdf2d779267fcca0d53a6131b6b021182f1
mango <esudatcom@gmail.com>
2023-09-10 23:21:57 +0900

    delete 추가

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ import org.springframework.stereotype.Controller;
 public class Article {
 
     @Id // 대표값을 지정! like a 주민등록번호
-    @GeneratedValue // 1, 2, 3 . . . 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1, 2, 3 . . . 자동 생성 어노테이션! // DB가 id를 자동 생성 어노테이션
     private Long id ;
 
     @Column
